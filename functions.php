29b274aacf8b0d5414e09787bb845c0d26b4070a
N M <contact@nm-webdesign.fr>
2020-05-04 13:12:14 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function str_random($length){
+    $alphabet = "0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+    return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
+}
+
+function str_token($length) {
+    $caract ="0123456789azertyuiopmlkjhgfdsqwxcvbnAZERTYUIOPMLKJHGFDSQWXCVBN";
+    return substr(str_shuffle(str_repeat($caract, $length)),0, $length);
+}
